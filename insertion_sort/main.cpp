9e7b3ca1c6f5a458740ed124ff8a384d07dd3985
ubyhzargam <ubyhzargam@gmail.com>
2023-12-04 15:48:41 +0530

    Initial Commit

---
+++ insertion_sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  insertion_sort
+//
+//  Created by Uby H on 04/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
