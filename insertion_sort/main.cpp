d5a79c19968b0edc332d8456f6824ebdb91781ca
ubyhzargam <ubyhzargam@gmail.com>
2023-12-05 22:01:49 +0530

    breakpoint to analyse

--- insertion_sort/main.cpp
+++ insertion_sort/main.cpp
@@ -1,14 +1,43 @@
-//
-//  main.cpp
-//  insertion_sort
-//
-//  Created by Uby H on 04/12/23.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
+void InsertionSort(int A[], int n)
+{
+    int j;
+    int x;
+    for(int i=1;i<n;i++)
+    {
+        x=A[i];
+        for(j=i-1;j>=0;j--)
+        {
+            if(A[j]>x)
+            {
+                A[j+1]=A[j];
+            }
+            else
+                break;
+        }
+        A[j+1]=x;
+    }
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void Display(int A[],int n)
+{
+    cout<<"The array elements in the sorted order are given by : "<<endl;
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements in the array to be sorted : "<<endl;
+    cin>>n;
+    cout<<"Enter the elements of the array to be sorted : "<<endl;
+    int A[50];
+    for(int i=0;i<n;i++)
+        cin>>A[i];
+    InsertionSort(A,n);
+    Display(A,n);
     return 0;
 }
